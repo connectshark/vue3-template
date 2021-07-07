e6cef17a3ab3b8986b12e7a442a3d69e146da15e
connectshark <bobhus394@gmail.com>
2021-07-07 15:01:04 +0800

    新增404路由

--- src/router/index.js
+++ src/router/index.js
@@ -9,7 +9,12 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: () => import('../views/home.vue')
-    }
+    },
+    {
+      path: '/:pathMatch(.*)*',
+      name: 'notFound',
+      component: () => import('../views/404.vue')
+    },
   ]
 })
 
