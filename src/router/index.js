104c2347a146e338f2773b4dc522d1e9d6014cbf
connectshark <bobhus394@gmail.com>
2021-07-07 14:23:00 +0800

    取消重新導向功能

--- src/router/index.js
+++ src/router/index.js
@@ -9,11 +9,6 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: () => import('../views/home.vue')
-    },
-    {
-      path: '/:pathMatch(.*)*',
-      name: 'notFound',
-      component: () => import('../views/404.vue')
     }
   ]
 })
