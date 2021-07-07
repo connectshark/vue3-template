f5e4cce68ff5ad3a4fddf9670b3d7c0bb9766235
connectshark <bobhus394@gmail.com>
2021-07-07 13:09:22 +0800

    初始化 template內容

---
+++ src/router/index.js
@@ -0,0 +1,15 @@
+const { createWebHistory, createRouter } = require('vue-router')
+
+const history = createWebHistory()
+
+const router = createRouter({
+  history,
+  routes: [
+    {
+      path: '/',
+      component: () => import('../views/home.vue')
+    }
+  ]
+})
+
+export default router
---
+++ src/views/home.vue
@@ -0,0 +1,3 @@
+<template>
+  <h2>Hi! 快速使用vue3做出網頁template</h2>
+</template>
