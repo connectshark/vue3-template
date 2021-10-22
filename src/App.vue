a40c7df1a81e58f2be3831c19b1ac27497ec96ea
connectshark <bobhus394@gmail.com>
2021-10-22 17:42:49 +0800

    更新常用內容配置

--- src/App.vue
+++ src/App.vue
@@ -12,11 +12,7 @@ export default {
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  font-family: 'Roboto Mono', 'Noto Sans TC', sans-serif;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import router from './router'
 import store from './store'
 import App from './App.vue'
 import i18n from './i18n'
+import './assets/css/reset.css'
 
 createApp(App)
   .use(i18n)
